adfe16ac2a3ef2c907d7105195d4777eb1434f67
Shashi Bhagat <skujur871@gmail.com>
2021-10-16 22:49:37 +0530

    initial setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('Here we go learning ts once again...');
+var number1 = 2.8;
+var number2 = 3;
+// console.log(number1 + number2);
+function add(arg1, arg2) {
+    return arg1 + arg2;
+}
+var result = add(number1, number2);
+console.log(result);
+var person = {
+    name: "Shashi Bhagat",
+    age: 27
+};
+console.log(person.name);
+var person2 = {
+    name: "Child",
+    age: 7,
+    hobbies: ['football', 'cricket']
+};
+console.log(person2);
--- app.ts
+++ app.ts
@@ -1,1 +1,40 @@
 console.log('Here we go learning ts once again...')
+
+const number1 = 2.8;
+const number2 = 3;
+
+// console.log(number1 + number2);
+
+function add(arg1: number, arg2: number) {
+    return arg1 + arg2;
+}
+
+const result = add(number1, number2);
+
+console.log(result)
+
+// Objects in TS
+// we have a generic type for objects is object.
+
+interface Person {
+    name: string;
+    age: number;
+    hobbies: string[];
+    roles: [number, string]; // tuples
+}
+
+const person = {
+    name: "Shashi Bhagat",
+    age: 27
+};
+
+console.log(person.name);
+
+const person2: Person = {
+    name: "Child",
+    age: 7,
+    hobbies: ['football', 'cricket'],
+    roles: [10, "admin"]
+};
+
+console.log(person2);
